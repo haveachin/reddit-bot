513f84e7acd687b66612e0bb3e7d9842b6e48091
Hendrik J. Schlehlein <hendrik.schlehlein@gmail.com>
2020-04-14 00:39:31 +0200

    Added basic implementation

---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"regexp"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	discord "github.com/bwmarrin/discordgo"
+)
+
+var redditPostPattern *regexp.Regexp
+
+func init() {
+	redditPostPattern = regexp.MustCompile(`https:\/\/www.reddit.com\/r\/(.+)\/comments\/(.+)\/.+`)
+}
+
+func main() {
+	discordSession, err := discordgo.New("Bot " + discordToken)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer discordSession.Close()
+	discordSession.AddHandler(onMessage)
+
+	if err := discordSession.Open(); err != nil {
+		log.Fatal("error opening connection,", err)
+	}
+
+	log.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+}
+
+func onMessage(s *discord.Session, m *discord.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	matches := redditPostPattern.FindStringSubmatch(m.Content)
+	if len(matches) < 3 {
+		return
+	}
+
+	redditPost, err := getPostData(matches[2])
+	if err != nil {
+		s.MessageReactionAdd(m.ChannelID, m.ID, "⚠️")
+		log.Println(err)
+		return
+	}
+
+	s.ChannelMessageDelete(m.ChannelID, m.ID)
+	s.ChannelMessageSendEmbed(m.ChannelID, &discord.MessageEmbed{
+		Title: redditPost.title,
+		Color: 16728833,
+		URL:   m.Content,
+		Author: &discord.MessageEmbedAuthor{
+			Name:    m.Author.Username,
+			IconURL: m.Author.AvatarURL(""),
+		},
+		Image: &discord.MessageEmbedImage{
+			URL: redditPost.imageURL,
+		},
+		Description: redditPost.subreddit,
+	})
+}
---
+++ request.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+const apiURL string = "https://www.reddit.com/%s/.json"
+
+type RedditPostJSON []struct {
+	Data struct {
+		Children []struct {
+			Data struct {
+				Title     string `json:"title"`
+				Subreddit string `json:"subreddit_name_prefixed"`
+				URL       string `json:"url"`
+			} `json:"data"`
+		} `json:"children"`
+	} `json:"data"`
+}
+
+type redditPost struct {
+	title     string
+	subreddit string
+	imageURL  string
+}
+
+func getPostData(postID string) (*redditPost, error) {
+	redditPostJSON := RedditPostJSON{}
+
+	url := fmt.Sprintf(apiURL, postID)
+	request, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	request.Header.Set("User-agent", "BonoBanani")
+
+	client := http.Client{}
+	resp, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&redditPostJSON); err != nil {
+		return nil, err
+	}
+
+	if len(redditPostJSON) <= 0 {
+		return nil, errors.New("bad response")
+	}
+
+	if len(redditPostJSON[0].Data.Children) <= 0 {
+		return nil, errors.New("bad response")
+	}
+
+	return &redditPost{
+		title:     redditPostJSON[0].Data.Children[0].Data.Title,
+		subreddit: redditPostJSON[0].Data.Children[0].Data.Subreddit,
+		imageURL:  redditPostJSON[0].Data.Children[0].Data.URL,
+	}, nil
+}
